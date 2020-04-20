1ee82a9a28fb47167c495f089a8fef4612afff75
Chris Lavington <chris.lavington@gmail.com>
2020-04-20 14:22:36 +0100

    added Vuetify

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from "vue";
+import Vue from 'vue'
 import { firestorePlugin } from 'vuefire'
-import App from "./App.vue";
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
 
 Vue.use(firestorePlugin)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  vuetify,
+  render: (h) => h(App)
+}).$mount('#app')
